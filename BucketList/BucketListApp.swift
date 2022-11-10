0517ef497108b8b323f6d705a5576ccfc7483f8d
Justin Wells <wellsmjustin@gmail.com>
2022-11-10 06:18:00 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Justin Wells on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
